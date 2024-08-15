527d56bff15b5e00f14515651c55be156516906b
MarcelineVQ <marcythevq@gmail.com>
2024-08-14 23:59:20 -0700

    init2

--- SilentBG.lua
+++ SilentBG.lua
@@ -4,24 +4,28 @@
 local bg_pattern = ".- %[%d+-%d+%] started!"
 local SilentBG = CreateFrame("Frame","SilentBG")
 
--- message add hook
-local add_message = DEFAULT_CHAT_FRAME.AddMessage
-function FilteredAddMessage(self,msg,a1,a2,a3,a4,a5,a6,a7,a8,a9)
-  if string.find(msg,bg_pattern) then return end
-  add_message(self,msg,a1,a2,a3,a4,a5,a6,a7,a8,a9)
-end
-
 SilentBG:RegisterEvent("CHAT_MSG_SYSTEM")
+SilentBG:RegisterEvent("CHAT_MSG_SAY")
 SilentBG:SetScript("OnEvent", function ()
   if event == "CHAT_MSG_SYSTEM" and string.find(arg1,bg_pattern) then
-    -- a system message for bg's has fired, temporary disallow printing of it
-    DEFAULT_CHAT_FRAME.AddMessage = FilteredAddMessage
+
+    -- print("BG POPPED, normal message supressed: "..arg1)
+
+    for i=1,NUM_CHAT_WINDOWS do
+      getglobal("ChatFrame"..i).OrigAddMessage = getglobal("ChatFrame"..i).AddMessage
+      getglobal("ChatFrame"..i).AddMessage = function (frame,msg,a1,a2,a3,a4,a5)
+        if string.find(msg,bg_pattern) then return end
+        getglobal("ChatFrame"..i).OrigAddMessage(frame,msg,a1,a2,a3,a4,a5)
+      end
+    end
 
     local elapsed = 0
     SilentBG:SetScript("OnUpdate", function ()
       elapsed = elapsed + arg1
       if elapsed > 0.2 then
-        DEFAULT_CHAT_FRAME.AddMessage = add_message
+        for i=1,NUM_CHAT_WINDOWS do
+          getglobal("ChatFrame"..i).AddMessage = getglobal("ChatFrame"..i).OrigAddMessage
+        end
         SilentBG:SetScript("OnUpdate", nil)
       end
     end)
