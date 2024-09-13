cba92cf99267fc5388ef5feab774f3348f731a4e
MarcelineVQ <marcythevq@gmail.com>
2024-09-13 01:07:10 -0700

    more lines

--- SilentServer.lua
+++ SilentServer.lua
@@ -1,20 +1,24 @@
 -- Name: SilentServer
 -- License: LGPL v2.1
 
--- no starting with ^ because these could be color coded
+-- not starting with ^ because these could be color coded,
 local patterns = {
   ".- %[%d+-%d+%] started!", -- battleground
   'Delete your WDB folder regularly',
   'If you want to help our project',
   'Keep up to date with',
   'We encourage everyone to change their password regularly',
-  'All gold transactions are ',
+  'All gold transactions are',
   'Tune in to Everlook Broadcasting',
   'Follow us on X',
+  'If you enjoy Mysteries of Azeroth',
 }
 
-local bg_pattern = ".- %[%d+-%d+%] started!"
 local SilentServer = CreateFrame("Frame","SilentServer")
+
+for i=1,NUM_CHAT_WINDOWS do
+  getglobal("ChatFrame"..i).OrigAddMessage = getglobal("ChatFrame"..i).AddMessage
+end
 
 local lock = false
 SilentServer:RegisterEvent("CHAT_MSG_SYSTEM")
@@ -27,13 +31,10 @@ SilentServer:SetScript("OnEvent", function ()
         pat = pattern
         break
       end
-      return -- no pattern
     end
-
-    print("FOO: "..arg1)
+    if not pat then return end -- no pattern
 
     for i=1,NUM_CHAT_WINDOWS do
-      getglobal("ChatFrame"..i).OrigAddMessage = getglobal("ChatFrame"..i).AddMessage
       getglobal("ChatFrame"..i).AddMessage = function (frame,msg,a1,a2,a3,a4,a5)
         if string.find(msg,pat) then return end
         getglobal("ChatFrame"..i).OrigAddMessage(frame,msg,a1,a2,a3,a4,a5)
